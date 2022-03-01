322d38283c4082855502b6b33a126a6ee757edc3
Shristi <shristigautam90@gmail.com>
2022-03-01 14:16:43 -0600

    question seven in functional way

--- AssignmentFive/js/QuestionSeven.js
+++ AssignmentFive/js/QuestionSeven.js
@@ -1,6 +1,7 @@
-// 7.	Write a function filterLongWords() that takes an array of words and an integer i and returns a new array containing only those words that were longer than i characters.
+// 7.	Write // Using Imperative Programminga function filterLongWords() that takes an array of words and an integer i and returns a new array containing only those words that were longer than i characters.
+// Using Imperative Programming
 function filterLongWords(arr, n) {
-    let newArr = []; //arrays should be declared with []
+    const newArr = []; //arrays should be declared with []
     for (let i = 0; i < arr.length; i++) {
         if (n < arr[i].length) {
             newArr.push(arr[i]);
@@ -10,3 +11,12 @@ function filterLongWords(arr, n) {
 }
 
 console.log("7) The new array with elements more than length 2 is: " + filterLongWords(["hi", "hello", "bye", "see", "oh", "you"], 2));
+//Another way to print: 
+// let elements = ["hi", "hello", "bye", "see", "oh", "you"];
+// filterLongWords(elements, 2).forEach(e => console.log(e));
+
+
+
+// Using Functional Programming
+const filterLongWords1 = (arr, n) => arr.filter(a => a.length > n);
+console.log("7) The new array with elements more than length 2 is: " + filterLongWords1(["hi", "hello", "bye", "see", "oh", "you"], 2));
